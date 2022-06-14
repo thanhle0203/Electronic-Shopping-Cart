78dfa8e379ddd996503594973edfd4cd078ae452
Thanh Le <thanhlevan0203@gmail.com>
2022-06-13 23:19:12 -0700

    Connected database to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+const express = require('express');
+const connectDB = require('./config/db');
 
-const express = require('express')
+connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log('Server running on port ${PORT}'))
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
