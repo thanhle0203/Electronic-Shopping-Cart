c12cf393a6b16d27eafbadc23398b1c2bfb85ff3
Thanh Le <thanhlevan0203@gmail.com>
2022-06-16 11:47:45 -0700

    Created Navbar and HomeScreen Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,31 @@
 
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
+// Components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      {/* Navbar */}
-      {/* SideDrawer */}
-      {/* Backdrop */}
-      {/* HomeScreen */}
-      {/* CartScreen */}
-    </div>
+    <Router>
+      < Navbar/ >
+      < HomeScreen />
+      < ProductScreen />
+      < CartScreen />
+      <main>
+        <Routes>
+          <Route exact path="/" component={HomeScreen}/>
+          <Route exact path="/product/:id" component={ProductScreen}/>
+          <Route exact path="/cart" component={CartScreen}/>
+        </Routes>
+      </main>
+
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import './HomeScreen.css';
+
+const HomeScreen = () => {
+  return (
+    <div className='homesreen'>Home Screen</div>
+  )
+}
+
+export default HomeScreen
