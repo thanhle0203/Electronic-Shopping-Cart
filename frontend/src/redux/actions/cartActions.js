abdc647f557e882d4d1a2be270c8cb029048feb4
Thanh Le <thanhlevan0203@gmail.com>
2022-06-19 12:38:12 -0700

    Created cartAction, productAction, productReduce Redux

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,6 +1,31 @@
 import * as ActionTypes from '../constants/cartConstants';
 import axios from 'axios';
 
-export const addToCart = {
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get('/api/products/${id}')
 
-}
+  dispatch ({
+    type: ActionTypes.ADD_TO_CART,
+    payload: {
+      product: data._id,
+      name: data.name,
+      imageUrl: data.imageUrl,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty
+    }
+  })
+
+  localStorage.setItem('cart', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = () => (dispatch, getState) => {
+  dispatch ({
+    type: ActionTypes.REMOVE_FROM_CART,
+    payload: id
+  })
+
+  localStorage.setItem('cart', JSON.stringify(getState().cart.cartItems));
+
+};
+
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,8 +2,15 @@ import {createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools} from 'redux-devtools-extension';
 
+// Reducers
+import { cartReducer } from './reducers/cartReducers';
+import {getProductDetailsReducer, getProductsDetails, getProductsReducer} from './reducers/productReducers';
+
 const reducer = combineReducers ({
-})
+  cart: cartReducer,
+  getProducts: getProductsReducer,
+  getProductsDetails: getProductDetailsReducer
+});
 
 
 const middleware = [thunk];
